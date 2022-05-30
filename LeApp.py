4f7b1bdbc83b983e864c8aa19d785dfb277f07ac
Innokentii Kozlov <innokentii.kozlov@tuni.fi>
2022-05-30 11:32:11 +0300

    A bit of command line

--- LeApp.py
+++ LeApp.py
@@ -7,25 +7,35 @@ from selenium import webdriver
 from ProjectParser.Parser import Parser
 
 
+
+
 def main():
+    
+    arguments = sys.argv.copy()
+    for index, arg in enumerate(sys.argv):
+        if arg == '-h' or arg == '-e' or arg == '-i':
+            arguments.insert(index+1, ' ')
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "f:i:e:")
+        opts, args = getopt.getopt(arguments[1:], "h:f:i:e:")
     except getopt.GetoptError as ex:
         Logger.error(f"You provided unsupportde option:\n\t{ex.msg}")
         return
 
-    driver = webdriver.Firefox()
-    driver.get("https://ainak.gitlab.io/leapp-app/") 
 
-    for option, argument in opts:    
+    for option, argument in opts:
+        if ('-h') in option:
+            Logger.info("You are usnig LeApp.\n\n\t -f <filename>.json - to load project from the file. \n\t -i - interactive mode. \n\t -e - to run examples.\n")
         if ('-f') in option:
+            driver = webdriver.Firefox()
+            driver.get("https://ainak.gitlab.io/leapp-app/") 
             parser = Parser(argument)
             parser.load_project()
             ToolBar.load_project(driver, parser)
         if ("-i") in option:
             Logger.warning("Interactive mode is not implemented. Need more ideas for that.")
         if ("-e") in option:
-            Logger.info("Go to SupportScripts and run Examples.py. I'll implement it a bit later, gonna go support Finland today's hockey match!")
+            Logger.warning("Go to SupportScripts and run Examples.py. I'll implement it a bit later, gonna go support Finland today's hockey match!")
 
 
 if __name__ == "__main__":
